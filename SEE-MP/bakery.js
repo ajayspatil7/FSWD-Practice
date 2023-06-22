60136bba34cbd8e41ba8c35f57b13829bac461d1
ajay <aspatil99451@gmail.com>
2023-06-22 08:18:21 +0530

    Fixed the display error :)

--- SEE-MP/bakery.js
+++ SEE-MP/bakery.js
@@ -1,24 +1,36 @@
-function getOutput(){
+document.addEventListener('DOMContentLoaded', function() {
+    var submitButton = document.getElementById('buttonSubmit');
+
+    submitButton.addEventListener('click', function(event) {
+        event.preventDefault();
+        getOutput();
+    });
+});
+
+function getOutput() {
     var outputFormat = "";
 
-    document.addEventListener('DOMContentLoaded', function() {
-        var form = document.getElementById('form-content');
-        var submitButton = document.getElementById('buttonSubmit');
-      
-        submitButton.addEventListener('click', function(event) {
-          event.preventDefault();
-      
-          var name = document.getElementById('name').value;
-          var email = document.getElementById('email').value;
-          var number = document.getElementById('number').value;
-          var city = document.getElementById('city').value;
-          var selectedLocation = document.querySelector('input[name="radioButton"]:checked');
-          var favoriteProducts = document.querySelectorAll('input[name="vehicle"]:checked');
+    var name = document.getElementById('name').value;
+    var email = document.getElementById('email').value;
+    var number = document.getElementById('number').value;
+    var city = document.getElementById('city').value;
 
-          outputFormat = "Hii " + userData.name + "!!\n" + "Your email id is " + userData.email + "\n" + "Your phone number is " + userData.number + "\n" + "You are from " + userData.city + "\n" + "You have selected " + userData.location + " as your location" + "\n" + "Your favorite products are " + userData.products + "\n";
-          document.getElementById('results').innerHTML = outputFormat;
+    var selectedLocation = document.querySelector('input[name="radioButton"]:checked');
+    var location = selectedLocation ? selectedLocation.value : '';
 
-        });
-      });
-      
+    var favoriteProducts = document.querySelectorAll('input[name="vehicle"]:checked');
+    var products = [];
+
+    favoriteProducts.forEach(function(product) {
+        products.push(product.value);
+    });
+
+    outputFormat = "Hi " + name + "!\n" +
+        "Your email id is " + email + "\n" +
+        "Your phone number is " + number + "\n" +
+        "You are from " + city + "\n" +
+        "You have selected " + location + " as your location" + "\n" +
+        "Your favorite products are " + products.join(', ') + "\n";
+
+    document.getElementById('results').textContent = outputFormat;
 }
